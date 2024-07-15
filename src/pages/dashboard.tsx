78e688c3a0fb6c3040f63a4b35eb90da2f649fbb
Eshaan Joshi <ejoshi@roambee.com>
2024-07-15 15:19:48 -0500

    moved types to dedicated file. refactored submite imei to use form action for refetch

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,58 +1,16 @@
-import { useState, Suspense } from "react"
-import { defer, Await, useLoaderData, useNavigate } from "react-router-dom"
+import {
+  useState,
+  Suspense
+} from "react"
+import {
+  defer,
+  Await,
+  useLoaderData,
+  useNavigate,
+  Form
+} from "react-router-dom"
 import DeltaDistanceHistogram from "../components/deltaDistanceHistogram";
-
-
-interface WifiData {
-  mac_address: string;
-  accuracy: number;
-  lat: number;
-  lng: number;
-  type: 'wifi';
-  used?: boolean;
-}
-
-interface GsmData {
-  cid: number;
-  lac: number;
-  mcc: number;
-  mnc: number;
-  accuracy: number;
-  lat: number;
-  lng: number;
-  type: 'gsm';
-}
-
-interface MsgGeo {
-  status: string;
-  tech: string;
-  lat: string;
-  lng: string;
-  reported_accuracy: number;
-  accuracy: number;
-  msg_source: string;
-  heterogenousLookup: boolean;
-}
-
-interface HeterogenousGeo {
-  lat: number;
-  lng: number;
-  accuracy: number;
-}
-
-export interface msgData {
-  id: number;
-  data: (WifiData | GsmData)[];
-  msg_geo: MsgGeo;
-  heterogenous_geo: HeterogenousGeo;
-  created_date: number;
-  bee_imei: string;
-  msg_uuid: string;
-  account_id: number;
-  msg_geo_distance: number;
-  heterogenous_geo_distance: number;
-  delta_distance: number;
-}
+import { msgData } from "../types";
 
 export const loader = async ({ request }: { request: Request }) => {
   const url = new URL(request.url);
@@ -80,11 +38,6 @@ export default function Dashboard() {
     setImei(e.target.value);
   };
 
-  const handleImeiSubmit = () => {
-    // Use the provided imei to navigate to the dashboard route with the query parameter
-    navigate(`/dashboard?imei=${imei}`);
-  };
-
   const handleSendToMap = () => {
     // Save the data in local storage using the IMEI as the key
     localStorage.setItem(`map-data-${imei}`, JSON.stringify(data.data));
@@ -95,22 +48,25 @@ export default function Dashboard() {
 
   return (
     <div className="">
-      {/*<h1 className="text-3xl font-bold mb-4">Dashboard</h1> */}
       <div className="mb-4 flex items-center">
-        <label className="mr-2 font-lg font-semibold">IMEI:</label>
-        <input
-          type="text"
-          value={imei}
-          onChange={handleImeiChange}
-          placeholder="Enter device imei..."
-          className="border rounded p-1 mr-2"
-        />
-        <button
-          onClick={handleImeiSubmit}
-          className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mr-2"
-        >
-          View IMEI
-        </button>
+        <Form method="get" action="/dashboard">
+          <label className="mr-2 font-lg font-semibold">IMEI:</label>
+          <input
+            type="text"
+            name="imei"
+            value={imei}
+            onChange={handleImeiChange}
+            placeholder="Enter device imei..."
+            className="border rounded p-1 mr-2"
+          />
+          <button
+            type="submit"
+            // onClick={handleImeiSubmit}
+            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mr-2"
+          >
+            View IMEI
+          </button>
+        </Form>
         <button
           onClick={handleSendToMap}
           className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
@@ -123,6 +79,6 @@ export default function Dashboard() {
           {(data: msgData[]) => <DeltaDistanceHistogram data={data} />}
         </Await>
       </Suspense>
-    </div>
+    </div >
   );
 }
