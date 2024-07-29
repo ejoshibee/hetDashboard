4194067b2af1dd184f3f77a925209b764236878f
Eshaan Joshi <ejoshi@roambee.com>
2024-07-29 15:23:35 -0700

    Setup dockerfiles and compose for containerized development. Base for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5175,
     proxy: {
       '/api': {
-        target: 'http://localhost:3007',
+        target: 'http://backend:3007',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, '')
