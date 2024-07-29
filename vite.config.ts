a3fcdd1e88c7fede9964356264b2ff56368a49bb
Eshaan Joshi <ejoshi@roambee.com>
2024-07-29 16:28:17 -0700

    configured vite.config to utilize modes for deployment instruction.

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5175,
-    proxy: {
-      '/api': {
-        target: 'http://backend:3007',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '')
+export default defineConfig(({ mode }) => {
+  // we have our NODE_ENV captured in mode.
+  const envObj = loadEnv(mode, process.cwd());
+  console.log(`Running process in mode: ${mode}`)
+  return {
+    plugins: [react()],
+    server: {
+      port: 5175,
+      proxy: {
+        '/api': {
+          target: mode === 'development' ? 'http://backend:3007' :
+            mode === 'staging' ? envObj.VITE_API_STAGING :
+            envObj.VITE_API_PROD,
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        },
       },
-    },
+    }
   }
 })
